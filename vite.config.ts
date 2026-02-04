2add32e51413ebcc8b25d060826ddda4ac31ad56
Ezekiel Ajayi <ezekiel_a@yahoo.com>
2026-02-04 13:25:22 +0100

    Updated Server Ip

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
-        host: '0.0.0.0',
+        host: '162.159.140.98',
         allowedHosts: ['dyxfront-heeak.ondigitalocean.app', 'dyxi.site']
       },
       plugins: [react()],
